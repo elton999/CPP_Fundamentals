50c78799fceb483cd23b17ffd9442b71526c6b79
Elton Silva <eltoncarsilva73@gmail.com>
2022-06-12 23:02:52 -0300

    gravity for Dapper Dasher

--- Dapper Dasher/DapperDasher.cpp
+++ Dapper Dasher/DapperDasher.cpp
@@ -2,16 +2,42 @@
 
 int main(void)
 {
-    int width{800};
-    int height{450};
+    const int windowWidth{800};
+    const int windowHeight{450};
 
-    InitWindow(width, height, "Dapper Dasher");
+    const int gravity{1};
+
+    const int width{50};
+    const int height{80};
+    int posY{windowHeight - height};
+
+    int velocity{0};
+    int jumpVelocity{-21};
+
+    bool isInAir{false};
+
+    InitWindow(windowWidth, windowHeight, "Dapper Dasher");
     SetTargetFPS(60);
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(WHITE);
+
+        isInAir = posY < windowHeight - height;
+
+        if (!isInAir)
+            velocity = 0;
+        else
+            velocity += gravity;
+
+        if (IsKeyPressed(KEY_SPACE) && !isInAir)
+            velocity += jumpVelocity;
+
+        posY += velocity;
+
+        DrawRectangle(windowWidth / 2, posY, width, height, BLUE);
+
         EndDrawing();
     }
     CloseWindow();
