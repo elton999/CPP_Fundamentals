1f68348a9794ea623aa6127e014634d21e08234c
Elton Silva <eltoncarsilva73@gmail.com>
2022-06-17 11:56:35 -0300

    character class and animation sprites

--- Classy Clash/main.cpp
+++ Classy Clash/main.cpp
@@ -1,6 +1,26 @@
 #include "raylib.h"
 #include "raymath.h"
 
+class Character
+{
+public:
+    Vector2 getWorldPos() { return worldPos; }
+
+private:
+    Texture2D texture;
+    Texture2D idle;
+    Texture2D run;
+    Vector2 screenPos;
+    Vector2 worldPos;
+    // 1 : facing right, -1 : facing left
+    int rightLeft{1};
+    // Animation variables
+    float runningTime{};
+    int frame{};
+    const int maxFrames{6};
+    const float updateTime{1.f / 12.f};
+};
+
 int main(void)
 {
     const int windowDimensions[2]{384, 384};
@@ -17,6 +37,17 @@ int main(void)
         windowDimensions[1] / 2.0f - (0.5f * (float)knight.height)};
     Rectangle knightSource{0, 0, (float)knight.width / 6.0f, (float)knight.height};
     float knightScale = 4.0;
+
+    // 1 : facing right, -1 : facing left
+    int rightLeft{1};
+
+    // Animation variables
+    Texture2D knight_idle = LoadTexture("characters/knight_idle_spritesheet.png");
+    Texture2D knight_run = LoadTexture("characters/knight_run_spritesheet.png");
+    float runningTime{};
+    int frame{};
+    const int maxFrames{6};
+    const float updateTime{1.f / 12.f};
 
     SetTargetFPS(60);
 
@@ -35,17 +66,31 @@ int main(void)
         if (IsKeyDown(KEY_S))
             direction.y += 1.0;
 
+        knight = knight_idle;
         if (Vector2Length(direction) != 0.0)
         {
             Vector2 movement = Vector2Normalize(direction);
             mapPos = Vector2Subtract(mapPos, Vector2Scale(movement, speed));
+            rightLeft = direction.x < 0.f ? -1 : 1;
+            knight = knight_run;
         }
 
         DrawTextureEx(map, mapPos, 0, 4.0, WHITE);
 
+        runningTime += GetFrameTime();
+        if (runningTime >= updateTime)
+        {
+            frame++;
+            runningTime = 0.f;
+            if (frame > maxFrames)
+                frame = 0;
+        }
+
+        knightSource.width = (float)knight.width / 6.0f * (float)rightLeft;
+        knightSource.x = knightSource.width * frame;
         Rectangle knightDest{knightPos.x, knightPos.y,
                              knightScale * (float)knight.width / 6.0f, knightScale * (float)knight.height};
-        DrawTexturePro(knight, knightSource, knightDest, {0, 0}, 0, WHITE);
+        DrawTexturePro(knight, knightSource, knightDest, Vector2{}, 0, WHITE);
 
         EndDrawing();
     }
@@ -53,7 +98,8 @@ int main(void)
     CloseWindow();
 
     UnloadTexture(map);
-    UnloadTexture(knight);
+    UnloadTexture(knight_run);
+    UnloadTexture(knight_idle);
 
     return 0;
 }
